bf13d8b5eb9946965f4aeb8c8f2d62236020dd70
DatSung <datsung.dev@gmail.com>
2024-10-30 15:51:24 +0700

    upload dll

--- FUMiniHotelManagement.Razor/obj/Debug/net6.0/FUMiniHotelManagement.Razor.AssemblyInfo.cs
+++ FUMiniHotelManagement.Razor/obj/Debug/net6.0/FUMiniHotelManagement.Razor.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FUMiniHotelManagement.Razor")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b355e254096dc7a322df13865d7b8810fa48d28e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+542b3ce507cf8381ccd6e8b0b40813eee01662da")]
 [assembly: System.Reflection.AssemblyProductAttribute("FUMiniHotelManagement.Razor")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FUMiniHotelManagement.Razor")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
